75031fd0a96734f583f5ac2126c0c4708499154e
Hafiz-Muhammad-Furqan <hafizm.furqan123@gmail.com>
2026-01-19 18:05:35 +0500

    generate: pre signed url from aws

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 dotenv.config();
 
 const app = express();
@@ -14,12 +16,31 @@ app.use(
 
 app.use(express.json());
 
-// Routes
+const client = new S3Client({
+  region: process.env.AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  },
+});
+
+const createPresignedUrlWithClient = ({ bucket, key }) => {
+  const command = new PutObjectCommand({ Bucket: bucket, Key: key });
+  return getSignedUrl(client, command, { expiresIn: 3600 });
+};
+
 app.get("/", (req, res) => {
   res.send("Welcome to s3 API");
 });
 
-// Server
+app.get("/api/generate-presigned-url", async (req, res) => {
+  const url = await createPresignedUrlWithClient({
+    bucket: process.env.S3_BUCKET,
+    key: "file1.png",
+  });
+  res.json({ url });
+});
+
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on PORT ${PORT}`);
